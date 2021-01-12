1fb14f686d89b93939a252655ce16a1eb1155abd
Michael Andrikanich <mandrikanich@gmail.com>
2021-01-12 21:48:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,10 +2,16 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+  #expect(output).to match(/Hello Dopey!/)
+  #expect(output).to match(/Hello Grumpy!/)
+  #expect(output).to match(/Hello Bashful!/)
 end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  #expect(output).to match(/1.*Dopey/)
+  #expect(output).to match(/2.*Grumpy/)
+  #expect(output).to match(/3.*Bashful/)
 end
